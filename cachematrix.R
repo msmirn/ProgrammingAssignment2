4a3ec1429b4de0c0d956f8c8d4e10123e6dc7333
msmirn <msmirn@gmail.com>
2014-10-25 23:30:25 +0400

    Assignment 2 final code.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Makes matrix which inverse can be cached internally
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    if ( is.na(x) == FALSE && all(x == y) ) {
+      message("same data, keeping the cache")
+    } else {
+      inv <<- NULL
+      x <<- y
+      message("data was changed, clearing the cache")
+    }
+  }
+  get <- function() x
+  setinv <- function(inv_arg) {
+    inv <<- inv_arg
+  }
+  getinv <- function() inv
+  list(set = set, 
+       get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x' using the cache if available
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv()
+  data <- x$get()
+  if( !is.null(i) ) {
+      message("getting cached data")
+      return(i)
+  }  
+  i <- solve(data, ...)
+  x$setinv(i)
+  i
 }
